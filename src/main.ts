52000cc097bd24774b25b2eba72866b6fa7bef0a
Efrain May <efrain_may_75@hotmail.com>
2023-02-07 00:52:48 -0600

    typescipt repaso

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@
 import './style.css'
 //import { name, age } from './bases/01-types'
 //import { charmander } from './bases/03-clases';
-import { charmander } from './bases/04-injection';
+//import { charmander } from './bases/04-injection';
+//import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2';
 
 charmander
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
 
-    <h1>Hello Vite ${charmander.name}</h1>
+    <h1>Hello Vite ${charmander.name} ${charmander.id}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
